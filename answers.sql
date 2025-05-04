29c34050b797f4729d0c075b20e71c9c8aae5066
Brian Sangura <sangura.bren@gmail.com>
2025-05-04 21:13:56 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -5,7 +5,6 @@ CREATE TABLE ProductDetail_1NF (
     Product VARCHAR(50)
 );
 
--- Inserting the normalized data
 INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
 VALUES 
     (101, 'John Doe', 'Laptop'),
@@ -25,19 +24,34 @@ CREATE TABLE Orders (
     CustomerName VARCHAR(100)
 );
 
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES 
+    (101, 'John Doe'),
+    (102, 'Jane Smith'),
+    (103, 'Emily Clark');
+
+    
+
 -- Create OrderProducts table (contains OrderID, Product, and Quantity)
-CREATE TABLE OrderProducts (
+CREATE TABLE Product (
     OrderID INT,
-    Product VARCHAR(50),
+    Product VARCHAR(100),
     Quantity INT,
     PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
 
--- Insert data into Orders table
-INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT OrderID, CustomerName FROM OrderDetails;
+-- Insert data into product table
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
 
--- Insert data into OrderProducts table
-INSERT INTO OrderProducts (OrderID, Product, Quantity)
-SELECT OrderID, Product, Quantity FROM OrderDetails;
+
+
+
+
